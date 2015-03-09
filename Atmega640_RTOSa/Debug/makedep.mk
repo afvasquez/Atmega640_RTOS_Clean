d23e9b3aa307d52497fceaadfa8eaaecd01b5b95
Andres Vasquez <andres.vasquez.ee@gmail.com>
2015-03-09 13:50:50 -0400

    Master Trunk of a Clean RTOS for Atmega640

---
+++ Atmega640_RTOSa/Debug/makedep.mk
@@ -0,0 +1,18 @@
+################################################################################
+# Automatically-generated file. Do not edit or delete the file
+################################################################################
+
+main.c
+
+Routines\Blinker.c
+
+RTOS\Source\list.c
+
+RTOS\Source\Portable\MemMang\heap_1.c
+
+RTOS\Source\Portable\port.c
+
+RTOS\Source\queue.c
+
+RTOS\Source\tasks.c
+
